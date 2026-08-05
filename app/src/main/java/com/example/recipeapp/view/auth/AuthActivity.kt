f2412b76779c22af83d577ecb2a03fd1517a0840
robamhmd <byyroba78@gmail.com>
2026-08-05 19:02:48 +0300

    Create and clean SplashFragment

--- app/src/main/java/com/example/recipeapp/view/auth/AuthActivity.kt
+++ app/src/main/java/com/example/recipeapp/view/auth/AuthActivity.kt
@@ -6,11 +6,16 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.recipeapp.R
 
 class AuthActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_auth)
 
-
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.authFragmentContainer, SplashFragment())
+                .commit()
+        }
     }
 }
