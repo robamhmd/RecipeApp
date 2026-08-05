21f492067043c5fccce92dfd3738af65afc7dd87
robamhmd <byyroba78@gmail.com>
2026-08-05 18:55:03 +0300

    Clean up AuthActivity template

--- app/src/main/java/com/example/recipeapp/view/auth/AuthActivity.kt
+++ app/src/main/java/com/example/recipeapp/view/auth/AuthActivity.kt
@@ -3,8 +3,6 @@ package com.example.recipeapp.view.auth
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.recipeapp.R
 
 class AuthActivity : AppCompatActivity() {
@@ -12,10 +10,7 @@ class AuthActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_auth)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+
     }
 }
